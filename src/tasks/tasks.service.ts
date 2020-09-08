f256d980c49a5a6c1b44fe8d762ea7e8f2b17e08
trugamr <6trugamr9@gmail.com>
2020-09-09 01:32:15 +0530

    updated tasks controller and service to support task status update and search using filters

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@nestjs/common'
 import { Task, TaskStatus } from './task.model'
 import { v1 as uuid } from 'uuid'
+import { CreateTaskDto } from './dto/create-task.dto'
+import { create } from 'domain'
+import { privateEncrypt } from 'crypto'
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto'
 
 @Injectable()
 export class TasksService {
@@ -10,7 +14,28 @@ export class TasksService {
     return this.tasks
   }
 
-  createTask(title: string, description: string): Task {
+  getTasksWithFitlers(filterDto: GetTasksFilterDto): Task[] {
+    const { status, search } = filterDto
+    let tasks = this.getAllTasks()
+    if (status) tasks = tasks.filter(task => task.status === status)
+    if (search)
+      tasks = tasks.filter(
+        task => task.title.includes(search) || task.description.includes(search)
+      )
+
+    return tasks
+  }
+
+  getTaskById(id: string): Task {
+    return this.tasks.find(task => task.id === id)
+  }
+
+  deleteTask(id: string): void {
+    this.tasks = this.tasks.filter(task => task.id !== id)
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto
     const task: Task = {
       id: uuid(),
       title,
@@ -19,6 +44,12 @@ export class TasksService {
     }
 
     this.tasks.push(task)
+    return task
+  }
+
+  updateTaskStatus(id: string, status: TaskStatus): Task {
+    const task = this.getTaskById(id)
+    task.status = status
     return task
   }
 }
