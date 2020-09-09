ecbfdddc82e9da498d2ec3ae9ac94d28f8aa516c
trugamr <6trugamr9@gmail.com>
2020-09-09 12:12:45 +0530

    created task repository, entity and refactored code to use postgresql

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,58 +1,45 @@
 import { Injectable, NotFoundException } from '@nestjs/common'
-import { Task, TaskStatus } from './task.model'
-import { v1 as uuid } from 'uuid'
 import { CreateTaskDto } from './dto/create-task.dto'
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto'
+import { TaskRepository } from './task.repository'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Task } from './task.entity'
+import { TaskStatus } from './task-status.enum'
+import { DeleteResult } from 'typeorm'
 
 @Injectable()
 export class TasksService {
-  private tasks: Task[] = []
+  constructor(
+    @InjectRepository(TaskRepository)
+    private taskRepository: TaskRepository
+  ) {}
 
-  getAllTasks(): Task[] {
-    return this.tasks
+  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    return this.taskRepository.getTasks(filterDto)
   }
 
-  getTasksWithFitlers(filterDto: GetTasksFilterDto): Task[] {
-    const { status, search } = filterDto
-    let tasks = this.getAllTasks()
-    if (status) tasks = tasks.filter(task => task.status === status)
-    if (search)
-      tasks = tasks.filter(
-        task => task.title.includes(search) || task.description.includes(search)
-      )
-
-    return tasks
-  }
-
-  getTaskById(id: string): Task {
-    const task = this.tasks.find(task => task.id === id)
+  async getTaskById(id: number): Promise<Task> {
+    const task = await this.taskRepository.findOne(id)
     if (!task) {
       throw new NotFoundException(`task with id "${id}" not found`)
     }
     return task
   }
 
-  deleteTask(id: string): void {
-    const foundTask = this.getTaskById(id)
-    this.tasks = this.tasks.filter(task => task.id !== foundTask.id)
+  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    return this.taskRepository.createTask(createTaskDto)
   }
 
-  createTask(createTaskDto: CreateTaskDto): Task {
-    const { title, description } = createTaskDto
-    const task: Task = {
-      id: uuid(),
-      title,
-      description,
-      status: TaskStatus.OPEN
-    }
-
-    this.tasks.push(task)
-    return task
+  async deleteTask(id: number): Promise<void> {
+    const deletedTask = await this.taskRepository.delete(id)
+    if (deletedTask.affected === 0)
+      throw new NotFoundException(`task with id "${id}" not found`)
   }
 
-  updateTaskStatus(id: string, status: TaskStatus): Task {
-    const task = this.getTaskById(id)
+  async updateTaskStatus(id: number, status: TaskStatus): Promise<Task> {
+    const task = await this.getTaskById(id)
     task.status = status
+    await task.save()
     return task
   }
 }
